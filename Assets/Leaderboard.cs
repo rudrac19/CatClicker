10caf9c953f6c13f7d39180cc48dd63d3dfc48de
Rudra Pratap Chauhan <rcpython2@gmail.com>
2024-03-01 15:58:39 -0500

    The leaderboard funtions now!!
    (beta 1.3.4)

---
+++ Assets/Leaderboard.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Dan.Main;
+
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField]
+    private List<TextMeshProUGUI> names;
+    [SerializeField]
+    private List<TextMeshProUGUI> scores;
+
+    private string publicLeaderboardKey = 
+        "68af9a9000669ece9ad6d7fc3126e58939f54b6eaf99435ad9e0b99ffc0bc83a";
+
+    void Start()
+    {
+        GetLeaderboard();
+    }
+
+    public void GetLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) => {
+            int loopLength = (msg.Length < names.Count) ? msg.Length : names.Count;
+            for (int i = 0; i < loopLength; i++)
+            {
+                names[i].text = msg[i].Username;
+                scores[i].text = msg[i].Score.ToString();
+            }
+        }));
+    }
+
+    public void SetLeaderboardEntry(string username, int score)
+    {
+        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, score, ((msg) =>
+        {
+            GetLeaderboard();
+        }));
+    }
+
+}
