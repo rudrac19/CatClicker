e4291f911b57f26e926ca1a97b107b190bc74632
Rudra Pratap Chauhan <rcpython2@gmail.com>
2024-03-10 15:12:55 -0400

    Home is all done!
    Home is all done with all the bugs fixed!
    (beta 1.3.10)

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] slides;
+    public string currentScreen;
+    public Animator navbarAnimator;
 
-    public void Quit()
+    void Start()
     {
-        Application.Quit();
+        slides[0].SetActive(false);
+        for (int i = 0; i < slides.Length; i++)
+        {
+            slides[i].SetActive(false);
+        }
+        slides[0].SetActive(true);
+        currentScreen = "Home";
     }
 
     public void ChangeSlide(int index)
@@ -19,5 +27,24 @@ public class GameManager : MonoBehaviour
             slides[i].SetActive(false);
         }
         slides[index].SetActive(true);
+    }
+
+    public void Navbar(string triggerName)
+    {
+        if (triggerName == "Home" && currentScreen == "Leaderboard")
+        {
+            navbarAnimator.SetTrigger(triggerName);
+            currentScreen = "Home";
+        }
+        if (triggerName == "Leaderboard" && currentScreen == "Home")
+        {
+            navbarAnimator.SetTrigger(triggerName);
+            currentScreen = "Leaderboard";
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
