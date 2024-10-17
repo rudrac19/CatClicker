f0ff44307fc6f5b04ba1f68543b3d6128aef599c
Rudra Pratap Chauhan <rcpython2@gmail.com>
2024-10-16 21:31:18 -0400

    Update CatSelector.cs
    (beta 1.4.2)

--- Assets/CatSelector.cs
+++ Assets/CatSelector.cs
@@ -8,37 +8,83 @@ public class CatSelector : MonoBehaviour
     public GameObject dogUI;
     public GameObject witchCatUI;
 
+    public GamObject[] selectedCatButtonUI
+
+
     public bool isDogPurchased;
     public bool isWitchCatPurchased;
 
-    public string selectedCat;
+    public int selectedCat;
     public string currentCatShownOnScreen;
 
+    void Start()
+    {
+        selectedCat = PlayerPrefs.GetInt("selectedCat", 0);
+    }
+
+
+    void Update()
+    {
+        // setting up the score
+        int score = AddScore.score;
+
+        // setting up the booleans
+        if (score >= 500 && score <= 1999)
+        {
+            isWitchCatPurchased = true;
+            isDogPurchased = false;
+        }
+
+        if (score >= 2000)
+        {
+            isDogPurchased = true;
+            isWitchCatPurchased = false;
+        }
+
+        // setting up the selected cat
+        if ()
+    }
+    
     public void ToWitchCat()
     {
-        if (isWitchCatPurchased)
-        {
-            currentCatShownOnScreen = "Witch";
-        }
+        
         catUI.SetActive(false);
         dogUI.SetActive(false);
         witchCatUI.SetActive(true);
     }
+
     public void ToCat()
     {
-        currentCatShownOnScreen = "Cat";
         catUI.SetActive(true);
         dogUI.SetActive(false);
         witchCatUI.SetActive(false);
     }
+
+    
     public void ToDog()
     {
-        if (isDogPurchased)
-        {
-            currentCatShownOnScreen = "Dog";
-        }
         catUI.SetActive(false);
         dogUI.SetActive(true);
         witchCatUI.SetActive(false);
     }
-}
+
+
+    public void SelectCat(int catIndex)
+    {
+        if (catIndex == 0)
+        {
+            selectedCat = catIndex;
+            PlayerPrefs.SetInt("selectedCat", selectedCat);
+        }
+        if (catIndex == 1 && isWitchCatPurchased)
+        {
+            selectedCat = catIndex;
+            PlayerPrefs.SetInt("selectedCat", selectedCat);
+        }
+        if (catIndex == 2 && isDogPurchased)
+        {
+            selectedCat = catIndex;
+            PlayerPrefs.SetInt("selectedCat", selectedCat);
+        }
+
+    }
